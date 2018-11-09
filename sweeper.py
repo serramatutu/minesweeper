7b8e20da6b305b54d8b5ed69509035c74824da64
serramatutu <lucasvvop@hotmail.com>
2018-11-09 10:03:12 -0200

    it's been a long day without you my friend
    -- adiciona argparse

--- sweeper.py
+++ sweeper.py
@@ -2,12 +2,26 @@
 # -*- coding: utf-8 -*- 
 
 from game import Sweeper
+import argparse
 
-def main():
-    field_size = (80, 40)
-    tile_size = (16, 16)
-    s = Sweeper(screen_size=Sweeper.screen_size(field_size, tile_size), field_size=field_size)
+def main(args):
+    field_size = (args.field_size, args.field_size)
+    tile_size = (args.tile_size, args.tile_size)
+    s = Sweeper(screen_size=Sweeper.screen_size(field_size, tile_size), 
+                field_size=field_size)
     s.run()
 
-if __name__ == "__main__": 
-    main()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--field-size', 
+                        help="the field size", 
+                        type=int, 
+                        default=16,
+                        dest='field_size')
+    parser.add_argument('--tile-size', 
+                        help="the tile size", 
+                        type=int, 
+                        default=32,
+                        dest='tile_size')
+
+    main(parser.parse_args())
